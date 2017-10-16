4ea6cd95a130c240e24b5a0ee9ae74f7ece71a46
naitRAM <rmansieh@gmail.com>
2017-10-16 15:34:19 -0400

    start work on logic

--- js/app.js
+++ js/app.js
@@ -1,24 +1,48 @@
-class Tile {
-	constructor(name){
-		this.name = document.getElementById(name);	
-	}
-	setX(){
-		this.name.style.backgroundPositionY="top";
-	}
-	setO(){
-		this.name.style.backgroundPositionY="bottom";
-	}
-	reset(){
-		this.name.style.backgroundPositionY="center";
-	}
-}
-
-var tile1 = new Tile("tile1");
-
-
 
 document.addEventListener("DOMContentLoaded", function(event){
 
-console.log("hello");
+var turn = "X";
+
+function changeTile() {
+	if (turn==="X"){
+		this.style.backgroundPositionY = "top";
+		checkRow();
+		turn = "O";
+		this.removeEventListener('click', changeTile);
+	}
+	else {
+		this.style.backgroundPositionY = "bottom";
+		turn = "X";
+		this.removeEventListener('click', changeTile);
+	}
+}
+
+function makeAllClickable(element){
+	element.addEventListener('click', changeTile);
+}
+
+function makeAllUnclickable(){
+	for (var i=1; i<10; i++) {
+	document.getElementById("tile"+i).removeEventListener('click', changeTile);	
+	} 
+}
+
+for (var i=1; i<10; i++) {
+	makeAllClickable(document.getElementById("tile"+i));
+}
+
+function checkRow() {
+	count = 0;
+	for (var i=0; i<3; i++) {
+	if (document.getElementsByClassName("row1")[i].style.backgroundPositionY === "0%"){
+	count++;
+	}
+	}
+	if (count===3) {
+		for (var i=0; i<3; i++) {
+		document.getElementsByClassName("row1")[i].style.backgroundColor = "pink";
+		}
+	}
+}
 
 });
