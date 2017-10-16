cead2bb108ac8851d3ab1c890f8ad64d563574cb
naitRAM <rmansieh@gmail.com>
2017-10-16 18:41:29 -0400

    add row logic for x's and o's and create a restart button

--- js/app.js
+++ js/app.js
@@ -3,22 +3,27 @@ document.addEventListener("DOMContentLoaded", function(event){
 
 var turn = "X";
 
+makeAllClickable();
+
 function changeTile() {
 	if (turn==="X"){
 		this.style.backgroundPositionY = "top";
-		checkRow();
+		checkRowsForX();
 		turn = "O";
 		this.removeEventListener('click', changeTile);
 	}
 	else {
 		this.style.backgroundPositionY = "bottom";
+		checkRowsForO();
 		turn = "X";
 		this.removeEventListener('click', changeTile);
 	}
 }
 
-function makeAllClickable(element){
-	element.addEventListener('click', changeTile);
+function makeAllClickable(){
+	for (var i=1; i<10; i++) {
+	document.getElementById("tile"+i).addEventListener('click', changeTile);	
+	} 
 }
 
 function makeAllUnclickable(){
@@ -27,11 +32,7 @@ function makeAllUnclickable(){
 	} 
 }
 
-for (var i=1; i<10; i++) {
-	makeAllClickable(document.getElementById("tile"+i));
-}
-
-function checkRow() {
+function checkRowsForX() {
 	var count = 0;
 	for (var i=0; i<3; i++) {
 	if (document.getElementsByClassName("row1")[i].style.backgroundPositionY === "0%"){
@@ -69,5 +70,59 @@ function checkRow() {
 		}
 	}
 }
+
+function checkRowsForO() {
+	var count = 0;
+	for (var i=0; i<3; i++) {
+	if (document.getElementsByClassName("row1")[i].style.backgroundPositionY === "100%"){
+	count++;
+	}
+	}
+	if (count===3) {
+		for (var i=0; i<3; i++) {
+		document.getElementsByClassName("row1")[i].style.backgroundColor = "pink";
+		makeAllUnclickable();
+		}
+	}
+	count = 0;
+	for (var i=0; i<3; i++) {
+	if (document.getElementsByClassName("row2")[i].style.backgroundPositionY === "100%"){
+	count++;
+	}
+	}
+	if (count===3) {
+		for (var i=0; i<3; i++) {
+		document.getElementsByClassName("row2")[i].style.backgroundColor = "pink";
+		makeAllUnclickable();
+		}
+	}
+	count = 0;
+	for (var i=0; i<3; i++) {
+	if (document.getElementsByClassName("row3")[i].style.backgroundPositionY === "100%"){
+	count++;
+	}
+	}
+	if (count===3) {
+		for (var i=0; i<3; i++) {
+		document.getElementsByClassName("row3")[i].style.backgroundColor = "pink";
+		makeAllUnclickable();
+		}
+	}
+}
+
+function resetTiles() {
+	for (var i=1; i<10; i++) {
+	document.getElementById("tile"+i).style.backgroundPosition = "";
+	document.getElementById("tile"+i).style.backgroundColor = "";
+	} 
+}
+
+
+
+document.getElementsByTagName("button")[0].addEventListener('click', function(){
+	resetTiles();
+	makeAllClickable();
+	turn = "X";
+})
 
 });
