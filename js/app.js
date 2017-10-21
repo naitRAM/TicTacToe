3dd55a127f9304a069d6b162c893d0abe5fa9cfe
naitRAM <rmansieh@gmail.com>
2017-10-21 01:50:50 -0400

    fix vs-computer bug and continue refactor

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 document.addEventListener("DOMContentLoaded", function(event){
 
 function computerPlays () {
+	if (endGame === false) {
+	turn = "O";
 	var tileList=[];
 	for (var i=1; i<10; i++){ 
 		if (document.getElementById("tile"+i).style.backgroundPositionY === "") {
@@ -14,152 +16,24 @@ function computerPlays () {
 	pickedTile.removeEventListener('click', changeTile);
 	checkRows("100%");
 	checkCols("100%");
-	checkCross("100%");
+	checkCrosses("100%");
 	turn = "X";
-}
-
-
-
-var turn = "X";
-
-makeAllClickable();
-
-function changeTile() {
-	if (turn==="X"){
-		this.style.backgroundPositionY = "top";
-		checkRows("0%");
-		checkCols("0%");
-		checkCross("0%");
-		turn = "O";
-		this.removeEventListener('click', changeTile);
-		computerPlays();
-	}
-	// else {
-	// 	this.style.backgroundPositionY = "bottom";
-	// 	checkRows("100%");
-	// 	checkCols("100%");
-	// 	checkCross("100%");
-	// 	turn = "X";
-	// 	this.removeEventListener('click', changeTile);
-	// }
-}
-
-function makeAllClickable(){
-	for (var i=1; i<10; i++) {
-	document.getElementById("tile"+i).addEventListener('click', changeTile);	
-	} 
-}
-
-function makeAllUnclickable(){
-	for (var i=1; i<10; i++) {
-	document.getElementById("tile"+i).removeEventListener('click', changeTile);	
-	} 
-}
-
-function checkRows(symbol) {
-	var count = 0;
-	for (var i=0; i<3; i++) {
-	if (document.getElementsByClassName("row1")[i].style.backgroundPositionY === symbol){
-	count++;
-	}
-	}
-	if (count===3) {
-		for (var i=0; i<3; i++) {
-		document.getElementsByClassName("row1")[i].style.backgroundColor = "pink";
-		makeAllUnclickable();
-		}
-	}
-	count = 0;
-	for (var i=0; i<3; i++) {
-	if (document.getElementsByClassName("row2")[i].style.backgroundPositionY === symbol){
-	count++;
-	}
-	}
-	if (count===3) {
-		for (var i=0; i<3; i++) {
-		document.getElementsByClassName("row2")[i].style.backgroundColor = "pink";
-		makeAllUnclickable();
-		}
-	}
-	count = 0;
-	for (var i=0; i<3; i++) {
-	if (document.getElementsByClassName("row3")[i].style.backgroundPositionY === symbol){
-	count++;
-	}
-	}
-	if (count===3) {
-		for (var i=0; i<3; i++) {
-		document.getElementsByClassName("row3")[i].style.backgroundColor = "pink";
-		makeAllUnclickable();
-		}
 	}
 }
+document.getElementsByTagName("button")[0].addEventListener('click', newGame);
 
-function checkCols(symbol) {
-	var count = 0;
-	for (var i=0; i<3; i++) {
-	if (document.getElementsByClassName("col1")[i].style.backgroundPositionY === symbol){
-	count++;
-	}
-	}
-	if (count===3) {
-		for (var i=0; i<3; i++) {
-		document.getElementsByClassName("col1")[i].style.backgroundColor = "pink";
-		makeAllUnclickable();
-		}
-	}
-	count = 0;
-	for (var i=0; i<3; i++) {
-	if (document.getElementsByClassName("col2")[i].style.backgroundPositionY === symbol){
-	count++;
-	}
-	}
-	if (count===3) {
-		for (var i=0; i<3; i++) {
-		document.getElementsByClassName("col2")[i].style.backgroundColor = "pink";
-		makeAllUnclickable();
-		}
-	}
-	count = 0;
-	for (var i=0; i<3; i++) {
-	if (document.getElementsByClassName("col3")[i].style.backgroundPositionY === symbol){
-	count++;
-	}
-	}
-	if (count===3) {
-		for (var i=0; i<3; i++) {
-		document.getElementsByClassName("col3")[i].style.backgroundColor = "pink";
-		makeAllUnclickable();
-		}
-	}
+var turn;
+var endGame;
+
+function newGame() {
+	resetTiles();
+	makeAllClickable();
+	turn = "X";
+	endGame = false;
 }
 
-function checkCross(symbol) {
-	var count=0;
-	for (var i=0; i<3; i++) {
-		if (document.getElementsByClassName("cross1")[i].style.backgroundPositionY===symbol){
-			count++;
-		}
-	}
-	if (count===3){
-		for (var i=0; i<3; i++) {
-		document.getElementsByClassName("cross1")[i].style.backgroundColor = "pink";
-		makeAllUnclickable();
-		}
-	}
-	count=0;
-	for (var i=0; i<3; i++) {
-		if (document.getElementsByClassName("cross2")[i].style.backgroundPositionY===symbol){
-			count++;
-	}
-	}
-	if (count===3){
-		for (var i=0; i<3; i++) {
-		document.getElementsByClassName("cross2")[i].style.backgroundColor = "pink";
-		makeAllUnclickable();
-		}
-	}
-}
+newGame();
+
 
 function resetTiles() {
 	for (var i=1; i<10; i++) {
@@ -168,10 +42,100 @@ function resetTiles() {
 	} 
 }
 
-document.getElementsByTagName("button")[0].addEventListener('click', function(){
-	resetTiles();
-	makeAllClickable();
-	turn = "X";
-})
+function makeAllClickable(){
+	for (var i=1; i<10; i++) {
+	document.getElementById("tile"+i).addEventListener('click', changeTile);	
+	} 
+}
+
+function changeTile() {
+	if (turn==="X"){
+		this.style.backgroundPositionY = "top";
+		checkRows("0%");
+		checkCols("0%");
+		checkCrosses("0%");
+		this.removeEventListener('click', changeTile);
+		computerPlays();
+	}
+	// else {
+	// 	this.style.backgroundPositionY = "bottom";
+	// 	checkRows("100%");
+	// 	checkCols("100%");
+	// 	checkCrosses("100%");
+	// 	turn = "X";
+	// 	this.removeEventListener('click', changeTile);
+	// }
+}
+
+function checkRow(rowNum, symbol){
+	var count = 0;
+	for (var i=0; i<3; i++) {
+	if (document.getElementsByClassName(rowNum)[i].style.backgroundPositionY === symbol){
+	count++;
+	}
+	}
+	if (count===3) {
+		for (var i=0; i<3; i++) {
+		document.getElementsByClassName(rowNum)[i].style.backgroundColor = "pink";
+		makeAllUnclickable();
+		endGame = true;
+		}
+	}
+}
+
+function checkCol(colNum, symbol){
+	var count = 0;
+	for (var i=0; i<3; i++) {
+	if (document.getElementsByClassName(colNum)[i].style.backgroundPositionY === symbol){
+	count++;
+	}
+	}
+	if (count===3) {
+		for (var i=0; i<3; i++) {
+		document.getElementsByClassName(colNum)[i].style.backgroundColor = "pink";
+		makeAllUnclickable();
+		endGame = true;
+		}
+	}
+}
+
+function checkCross(crossNum, symbol){
+	var count=0;
+	for (var i=0; i<3; i++) {
+		if (document.getElementsByClassName(crossNum)[i].style.backgroundPositionY===symbol){
+			count++;
+		}
+	}
+	if (count===3){
+		for (var i=0; i<3; i++) {
+		document.getElementsByClassName(crossNum)[i].style.backgroundColor = "pink";
+		makeAllUnclickable();
+		endGame = true;
+		}
+	}
+}
+
+function checkRows(symbol) {
+	checkRow("row1", symbol);
+	checkRow("row2", symbol);
+	checkRow("row3", symbol);
+}
+
+function checkCols(symbol) {
+	checkCol("col1", symbol);
+	checkCol("col2", symbol);
+	checkCol("col3", symbol);
+}
+
+function checkCrosses(symbol) {
+	checkCross("cross1", symbol);
+	checkCross("cross2", symbol);
+}
+
+function makeAllUnclickable(){
+	for (var i=1; i<10; i++) {
+	document.getElementById("tile"+i).removeEventListener('click', changeTile);	
+	} 
+}
 
 });
